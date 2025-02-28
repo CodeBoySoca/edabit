2c79246cd731695d201f3d9d41fcd4f628773b9f
web-developer-account <rockylatchman3624@gmail.com>
2025-02-28 08:07:52 -0800

    Frequency Distribution code challenge

---
+++ js/ex-228-02.js
@@ -0,0 +1,27 @@
+/**
+
+Frequency Distribution
+URL: https://edabit.com/challenge/uScx6aGXmNu27NYWz
+Challenge Description:
+
+Create a function that returns the frequency distribution of an array. This function should
+return an object, where the keys are the unique elements and the values are the frequency in
+which those elements occur.
+
+*/
+
+
+function getFrequencies(arr){
+    resultObj = {};
+    arr.map((x) => {
+      if(resultObj.hasOwnProperty(x)){
+          resultObj[x] += 1;
+      } else {
+          resultObj[x] = 1;
+      }
+    })
+  return resultObj;
+}
+
+const freq = getFrequencies(["A", "B", "A", "A", "A"])
+console.log(freq);
